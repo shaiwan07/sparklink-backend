03285cbddaa97c72049c488bb9e0daf27eb58dad
shaiwan-shaikh07 <shaiwan.Shaikh@techbrainscorp.com>
2026-03-24 23:30:50 +0530

    fixed tables names

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -5,7 +5,8 @@
  * @returns {string}
  */
 function generateOTP() {
-  return Math.floor(100000 + Math.random() * 900000).toString();
+  // return Math.floor(100000 + Math.random() * 900000).toString();
+  return 123456;
 }
 
 module.exports = {
